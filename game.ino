42e7c4018aa462e4e4455399f0e9be59f2cb4153
nainghtetlinn <naing.work.it@gmail.com>
2025-02-03 22:05:01 +0630

    fix bug

--- game.ino
+++ game.ino
@@ -1,23 +1,26 @@
 // player A
+const int playerA_abcPin = 34;
 const int ledPinaa = 2;
 const int ledPinab = 13;
 const int ledPinac = 14;
-const int playerA_abcPin = 34;
 // player B
-const int ledPinba = 2;
-const int ledPinbb = 13;
-const int ledPinbc = 14;
-const int playerB_abcPin = 34;
+const int playerB_abcPin = 32;
+const int ledPinba = 25;
+const int ledPinbb = 26;
+const int ledPinbc = 27;
 // player C
-const int ledPinca = 2;
-const int ledPincb = 13;
-const int ledPincc = 14;
-const int playerC_abcPin = 34;
+const int playerC_abcPin = 33;
+const int ledPinca = 18;
+const int ledPincb = 19;
+const int ledPincc = 21;
 
 // game states
-String playerA = "0";
-String playerB = "0";
-String playerC = "0";
+String playerA = "";
+String playerB = "";
+String playerC = "";
+int playerA_value;
+int playerB_value;
+int playerC_value;
 
 String receivedData = "";
 bool ableToAns = false;
@@ -35,8 +38,6 @@ void setup()
 
 void loop()
 {
-    updateLeds();
-
     if (Serial.available())
     {
         receivedData = Serial.readStringUntil('\n'); // Read data until newline
@@ -45,6 +46,10 @@ void loop()
             if (receivedData.indexOf("choice:True") != -1)
             {
                 ableToAns = true;
+                playerA = "";
+                playerB = "";
+                playerC = "";
+                counter = 0;
                 if (receivedData.indexOf("choice:True1") != -1)
                     playerCount = 1;
                 else if (receivedData.indexOf("choice:True2") != -1)
@@ -61,34 +66,37 @@ void loop()
 
     if (ableToAns)
     {
-        int playerA_value = analogRead(playerA_abcPin);
-        int playerB_value = analogRead(playerB_abcPin);
-        int playerC_value = analogRead(playerC_abcPin);
-
-        // message = "AR:{f:[A],s:[0],t:[0],c:[0]}";
-        // player A answer
-        updatePlayerState(playerA, playerA_value);
-
-        // player B answer
-        updatePlayerState(playerB, playerB_value);
-
-        // player C answer
-        updatePlayerState(playerC, playerC_value);
-
-        String message = "AR:{f:[" + playerA + "],s:[" + playerB + "],t:[" + playerC + "],c:[" + counter + "]}";
-
-        Serial.println(message);
+        if (playerCount >= 1 && playerA == "")
+        {
+            // player A answer
+            playerA_value = analogRead(playerA_abcPin);
+            updatePlayerState(playerA, playerA_value);
+        }
+        if (playerCount >= 2 && playerB == "")
+        {
+            // player B answer
+            playerB_value = analogRead(playerB_abcPin);
+            updatePlayerState(playerB, playerB_value);
+        }
+        if (playerCount == 3 && playerC == "")
+        {
+            // player C answer
+            playerC_value = analogRead(playerC_abcPin);
+            updatePlayerState(playerC, playerC_value);
+        }
 
         if (playerCount == counter)
         {
             ableToAns = false;
         }
     }
+
+    updateLeds();
 }
 
 void updatePlayerState(String &player, int value)
 {
-    if (player == "0" && value < 4000)
+    if (player == "" && value < 4000)
     {
         counter++;
 
@@ -98,6 +106,9 @@ void updatePlayerState(String &player, int value)
             player = "B";
         else if (value >= 3600)
             player = "C";
+
+        // message = "AR:{f:[A],s:[],t:[],c:[0]}";
+        Serial.println("AR:{f:[" + playerA + "],s:[" + playerB + "],t:[" + playerC + "],c:[" + counter + "]}");
     }
 }
 
